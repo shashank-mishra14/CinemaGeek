46847ac5ea8b0fb81e01973755fefd430c033ea9
shashank01sj <shashank@sportjacks.com>
2024-07-17 17:25:59 +0530

    changes

---
+++ src/main/java/com/movieSystem/CinemaGeek/CinemaGeekApplication.java
@@ -0,0 +1,13 @@
+package com.movieSystem.CinemaGeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaGeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaGeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movieSystem/CinemaGeek/CinemaGeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieSystem.CinemaGeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaGeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
