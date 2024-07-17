86f479c91a4970dd82958ee9fd65297b99f3b1be
shashank01sj <shashank@sportjacks.com>
2024-07-17 17:39:36 +0530

    model for movie completed

---
+++ src/main/java/com/movieSystem/CinemaGeek/model/Movie.java
@@ -0,0 +1,41 @@
+package com.movieSystem.CinemaGeek.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId _id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> backdrops;
+    private List<String> generes;
+    @DocumentReference
+    private List<String> reviews;
+
+    public Movie(ObjectId id, String imdbId, String title, String releaseDate, String trailerLink, String poster, List<String> backdrops, List<String> generes) {
+        this._id = id;
+        this.imdbId = imdbId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.trailerLink = trailerLink;
+        this.poster = poster;
+        this.backdrops = backdrops;
+        this.generes = generes;
+    }
+}
